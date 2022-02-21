0d45dc1b070d0648b4d3e99a3ed683a44b18e46e
Uladzimir Praca <Uladzimir Praca@PARUS>
2022-02-21 22:46:20 +0300

    Добавьте файлы проекта.

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 10;
+            Console.WriteLine("Переменная variable со значением: " + variable);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = Math.PI;
+            double e = Math.Sqrt(2);
+
+            Console.WriteLine("Константа PI: " + PI);
+            Console.WriteLine("Константа Пифагора: " + e);
+            Console.ReadKey();
+        }
+    }
+}
